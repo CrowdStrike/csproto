2cf177e5af9015d793512122276299956235d8ae
Dan Sexton <dan.sexton@crowdstrike.com>
2024-10-28 12:56:45 -0400

    feat: added proto3 optional support to fastmarshal

--- example/proto3/googlev2/googlev2_proto3_example.pb.fm.go
+++ example/proto3/googlev2/googlev2_proto3_example.pb.fm.go
@@ -3376,6 +3376,383 @@ func (m *OneOfs) Unmarshal(p []byte) error {
 }
 
 //------------------------------------------------------------------------------
+// Custom Protobuf size/marshal/unmarshal code for Optionals
+
+// Size calculates and returns the size, in bytes, required to hold the contents of m using the Protobuf
+// binary encoding.
+func (m *Optionals) Size() int {
+	// nil message is always 0 bytes
+	if m == nil {
+		return 0
+	}
+	// return cached size, if present
+	if csz := int(atomic.LoadInt32(&m.sizeCache)); csz > 0 {
+		return csz
+	}
+	// calculate and cache
+	var sz, l int
+	_ = l // avoid unused variable
+
+	// OptionalString (string,optional)
+	if m.OptionalString != nil {
+		l = len(*m.OptionalString)
+		sz += csproto.SizeOfTagKey(2) + csproto.SizeOfVarint(uint64(l)) + l
+	}
+	// OptionalBool (bool,optional)
+	if m.OptionalBool != nil {
+		sz += csproto.SizeOfTagKey(3) + 1
+	}
+	// OptionalInt32 (int32,optional)
+	if m.OptionalInt32 != nil {
+		sz += csproto.SizeOfTagKey(4) + csproto.SizeOfVarint(uint64(*m.OptionalInt32))
+	}
+	// OptionalInt64 (int64,optional)
+	if m.OptionalInt64 != nil {
+		sz += csproto.SizeOfTagKey(5) + csproto.SizeOfVarint(uint64(*m.OptionalInt64))
+	}
+	// OptionalUint32 (uint32,optional)
+	if m.OptionalUint32 != nil {
+		sz += csproto.SizeOfTagKey(6) + csproto.SizeOfVarint(uint64(*m.OptionalUint32))
+	}
+	// OptionalUint64 (uint64,optional)
+	if m.OptionalUint64 != nil {
+		sz += csproto.SizeOfTagKey(7) + csproto.SizeOfVarint(uint64(*m.OptionalUint64))
+	}
+	// OptionalSint32 (sint32,optional)
+	if m.OptionalSint32 != nil {
+		sz += csproto.SizeOfTagKey(8) + csproto.SizeOfZigZag(uint64(*m.OptionalSint32))
+	}
+	// OptionalSint64 (sint64,optional)
+	if m.OptionalSint64 != nil {
+		sz += csproto.SizeOfTagKey(9) + csproto.SizeOfZigZag(uint64(*m.OptionalSint64))
+	}
+	// OptionalFixed32 (fixed32,optional)
+	if m.OptionalFixed32 != nil {
+		sz += csproto.SizeOfTagKey(10) + 4
+	}
+	// OptionalFixed64 (fixed64,optional)
+	if m.OptionalFixed64 != nil {
+		sz += csproto.SizeOfTagKey(11) + 8
+	}
+	// OptionalSfixed32 (sfixed32,optional)
+	if m.OptionalSfixed32 != nil {
+		sz += csproto.SizeOfTagKey(12) + 4
+	}
+	// OptionalSfixed64 (sfixed64,optional)
+	if m.OptionalSfixed64 != nil {
+		sz += csproto.SizeOfTagKey(13) + 8
+	}
+	// OptionalFloat (float,optional)
+	if m.OptionalFloat != nil {
+		sz += csproto.SizeOfTagKey(14) + 4
+	}
+	// OptionalDouble (double,optional)
+	if m.OptionalDouble != nil {
+		sz += csproto.SizeOfTagKey(15) + 8
+	}
+	// OptionalEnum (enum,optional)
+	if m.OptionalEnum != nil {
+		sz += csproto.SizeOfTagKey(16) + csproto.SizeOfVarint(uint64(*m.OptionalEnum))
+	}
+	// OptionalBytes (bytes,optional)
+	if l = len(m.OptionalBytes); l > 0 {
+		sz += csproto.SizeOfTagKey(17) + csproto.SizeOfVarint(uint64(l)) + l
+	}
+	// OptionalMessage (message,optional)
+	if m.OptionalMessage != nil {
+		l = csproto.Size(m.OptionalMessage)
+		sz += csproto.SizeOfTagKey(18) + csproto.SizeOfVarint(uint64(l)) + l
+	}
+	// cache the size so it can be re-used in Marshal()/MarshalTo()
+	atomic.StoreInt32(&m.sizeCache, int32(sz))
+	return sz
+}
+
+// Marshal converts the contents of m to the Protobuf binary encoding and returns the result or an error.
+func (m *Optionals) Marshal() ([]byte, error) {
+	siz := m.Size()
+	if siz == 0 {
+		return []byte{}, nil
+	}
+	buf := make([]byte, siz)
+	err := m.MarshalTo(buf)
+	return buf, err
+}
+
+// MarshalTo converts the contents of m to the Protobuf binary encoding and writes the result to dest.
+func (m *Optionals) MarshalTo(dest []byte) error {
+	// nil message == no-op
+	if m == nil {
+		return nil
+	}
+	var (
+		enc    = csproto.NewEncoder(dest)
+		buf    []byte
+		err    error
+		extVal interface{}
+	)
+	// ensure no unused variables
+	_ = enc
+	_ = buf
+	_ = err
+	_ = extVal
+
+	// OptionalString (2,string,optional)
+	if m.OptionalString != nil {
+		enc.EncodeString(2, *m.OptionalString)
+	}
+	// OptionalBool (3,bool,optional)
+	if m.OptionalBool != nil {
+		enc.EncodeBool(3, *m.OptionalBool)
+	}
+	// OptionalInt32 (4,int32,optional)
+	if m.OptionalInt32 != nil {
+		enc.EncodeInt32(4, *m.OptionalInt32)
+	}
+	// OptionalInt64 (5,int64,optional)
+	if m.OptionalInt64 != nil {
+		enc.EncodeInt64(5, *m.OptionalInt64)
+	}
+	// OptionalUint32 (6,uint32,optional)
+	if m.OptionalUint32 != nil {
+		enc.EncodeUInt32(6, *m.OptionalUint32)
+	}
+	// OptionalUint64 (7,uint64,optional)
+	if m.OptionalUint64 != nil {
+		enc.EncodeUInt64(7, *m.OptionalUint64)
+	}
+	// OptionalSint32 (8,sint32,optional)
+	if m.OptionalSint32 != nil {
+		enc.EncodeSInt32(8, *m.OptionalSint32)
+	}
+	// OptionalSint64 (9,sint64,optional)
+	if m.OptionalSint64 != nil {
+		enc.EncodeSInt64(9, *m.OptionalSint64)
+	}
+	// OptionalFixed32 (10,fixed32,optional)
+	if m.OptionalFixed32 != nil {
+		enc.EncodeFixed32(10, *m.OptionalFixed32)
+	}
+	// OptionalFixed64 (11,fixed64,optional)
+	if m.OptionalFixed64 != nil {
+		enc.EncodeFixed64(11, *m.OptionalFixed64)
+	}
+	// OptionalSfixed32 (12,sfixed32,optional)
+	enc.EncodeFixed32(12, uint32(*m.OptionalSfixed32))
+	// OptionalSfixed64 (13,sfixed64,optional)
+	enc.EncodeFixed64(13, uint64(*m.OptionalSfixed64))
+	// OptionalFloat (14,float,optional)
+	if m.OptionalFloat != nil {
+		enc.EncodeFloat32(14, *m.OptionalFloat)
+	}
+	// OptionalDouble (15,double,optional)
+	if m.OptionalDouble != nil {
+		enc.EncodeFloat64(15, *m.OptionalDouble)
+	}
+	// OptionalEnum (16,enum,optional)
+	if m.OptionalEnum != nil {
+		enc.EncodeInt32(16, int32(*m.OptionalEnum))
+	}
+	// OptionalBytes (17,bytes,optional)
+	if len(m.OptionalBytes) > 0 {
+		enc.EncodeBytes(17, m.OptionalBytes)
+	}
+	// OptionalMessage (18,message,optional)
+	if m.OptionalMessage != nil {
+		if err = enc.EncodeNested(18, m.OptionalMessage); err != nil {
+			return fmt.Errorf("unable to encode message data for field 'optional_message' (tag=18): %w", err)
+		}
+	}
+	return nil
+}
+
+// Unmarshal decodes a binary encoded Protobuf message from p and populates m with the result.
+func (m *Optionals) Unmarshal(p []byte) error {
+	m.Reset()
+	if len(p) == 0 {
+		return nil
+	}
+	dec := csproto.NewDecoder(p)
+	for dec.More() {
+		tag, wt, err := dec.DecodeTag()
+		if err != nil {
+			return err
+		}
+		switch tag {
+		case 2: // OptionalString (string,optional)
+			if wt != csproto.WireTypeLengthDelimited {
+				return fmt.Errorf("incorrect wire type %v for field 'optional_string' (tag=2), expected 2 (length-delimited)", wt)
+			}
+			if s, err := dec.DecodeString(); err != nil {
+				return fmt.Errorf("unable to decode string value for field 'optional_string' (tag=2): %w", err)
+			} else {
+				m.OptionalString = csproto.String(s)
+			}
+
+		case 3: // OptionalBool (bool,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_bool' (tag=3), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeBool(); err != nil {
+				return fmt.Errorf("unable to decode boolean value for field 'optional_bool' (tag=3): %w", err)
+			} else {
+				m.OptionalBool = csproto.Bool(v)
+			}
+		case 4: // OptionalInt32 (int32,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_int32' (tag=4), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeInt32(); err != nil {
+				return fmt.Errorf("unable to decode int32 value for field 'optional_int32' (tag=4): %w", err)
+			} else {
+				m.OptionalInt32 = csproto.Int32(v)
+			}
+		case 5: // OptionalInt64 (int64,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_int64' (tag=5), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeInt64(); err != nil {
+				return fmt.Errorf("unable to decode int64 value for field 'optional_int64' (tag=5): %w", err)
+			} else {
+				m.OptionalInt64 = csproto.Int64(v)
+			}
+		case 6: // OptionalUint32 (uint32,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_uint32' (tag=6), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeUInt32(); err != nil {
+				return fmt.Errorf("unable to decode int32 value for field 'optional_uint32' (tag=6): %w", err)
+			} else {
+				m.OptionalUint32 = csproto.Uint32(v)
+			}
+		case 7: // OptionalUint64 (uint64,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_uint64' (tag=7), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeUInt64(); err != nil {
+				return fmt.Errorf("unable to decode uint64 value for field 'optional_uint64' (tag=7): %w", err)
+			} else {
+				m.OptionalUint64 = csproto.Uint64(v)
+			}
+		case 8: // OptionalSint32 (sint32,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_sint32' (tag=8), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeSInt32(); err != nil {
+				return fmt.Errorf("unable to decode sint32 value for field 'optional_sint32' (tag=8): %w", err)
+			} else {
+				m.OptionalSint32 = csproto.Int32(v)
+			}
+		case 9: // OptionalSint64 (sint64,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_sint64' (tag=9), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeSInt64(); err != nil {
+				return fmt.Errorf("unable to decode sint64 value for field 'optional_sint64' (tag=9): %w", err)
+			} else {
+				m.OptionalSint64 = csproto.Int64(v)
+			}
+		case 10: // OptionalFixed32 (fixed32,optional)
+			if wt != csproto.WireTypeFixed32 {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_fixed32' (tag=10), expected 5 (32-bit)", wt)
+			}
+			if v, err := dec.DecodeFixed32(); err != nil {
+				return fmt.Errorf("unable to decode uint32 value for field 'optional_fixed32' (tag=10): %w", err)
+			} else {
+				m.OptionalFixed32 = csproto.Uint32(v)
+			}
+		case 11: // OptionalFixed64 (fixed64,optional)
+			if wt != csproto.WireTypeFixed64 {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_fixed64' (tag=11), expected 1 (64-bit)", wt)
+			}
+			if v, err := dec.DecodeFixed64(); err != nil {
+				return fmt.Errorf("unable to decode uint64 value for field 'optional_fixed64' (tag=11): %w", err)
+			} else {
+				m.OptionalFixed64 = csproto.Uint64(v)
+			}
+		case 12: // OptionalSfixed32 (sfixed32,optional)
+			if wt != csproto.WireTypeFixed32 {
+				return fmt.Errorf("incorrect wire type %v for field 'optional_sfixed32' (tag=12), expected 5 (32-bit)", wt)
+			}
+			if v, err := dec.DecodeFixed32(); err != nil {
+				return fmt.Errorf("unable to decode sfixed32 value for field 'optional_sfixed32' (tag=12): %w", err)
+			} else {
+				iv := int32(v)
+				m.OptionalSfixed32 = &iv
+			}
+
+		case 13: // OptionalSfixed64 (sfixed64,optional)
+			if wt != csproto.WireTypeFixed64 {
+				return fmt.Errorf("incorrect wire type %v for field 'optional_sfixed64' (tag=13), expected 1 (64-bit)", wt)
+			}
+			if v, err := dec.DecodeFixed64(); err != nil {
+				return fmt.Errorf("unable to decode sfixed64 value for field 'optional_sfixed64' (tag=13): %w", err)
+			} else {
+				iv := int64(v)
+				m.OptionalSfixed64 = &iv
+			}
+
+		case 14: // OptionalFloat (float,optional)
+			if wt != csproto.WireTypeFixed32 {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_float' (tag=14), expected 5 (32-bit)", wt)
+			}
+			if v, err := dec.DecodeFloat32(); err != nil {
+				return fmt.Errorf("unable to decode float value for field 'optional_float' (tag=14): %w", err)
+			} else {
+				m.OptionalFloat = csproto.Float32(v)
+			}
+		case 15: // OptionalDouble (double,optional)
+			if wt != csproto.WireTypeFixed64 {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_double' (tag=15), expected 1 (64-bit)", wt)
+			}
+			if v, err := dec.DecodeFloat64(); err != nil {
+				return fmt.Errorf("unable to decode double value for field 'optional_double' (tag=15): %w", err)
+			} else {
+				m.OptionalDouble = csproto.Float64(v)
+			}
+		case 16: // OptionalEnum (enum,optional)
+			if wt != csproto.WireTypeVarint {
+				return fmt.Errorf("incorrect wire type %v for tag field 'optional_enum' (tag=16), expected 0 (varint)", wt)
+			}
+			if v, err := dec.DecodeInt32(); err != nil {
+				return fmt.Errorf("unable to decode int32 enum value for field 'optional_enum' (tag=16): %w", err)
+			} else {
+				ev := EventType(v)
+				m.OptionalEnum = &ev
+			}
+		case 17: // OptionalBytes (bytes,optional)
+
+			if wt != csproto.WireTypeLengthDelimited {
+				return fmt.Errorf("incorrect wire type %v for field 'optional_bytes' (tag=17), expected 2 (length-delimited)", wt)
+			}
+			if b, err := dec.DecodeBytes(); err != nil {
+				return fmt.Errorf("unable to decode bytes value for field 'optional_bytes' (tag=17): %w", err)
+			} else {
+				m.OptionalBytes = b
+			}
+
+		case 18: // OptionalMessage (message,optional)
+			if wt != csproto.WireTypeLengthDelimited {
+				return fmt.Errorf("incorrect wire type %v for field 'optional_message' (tag=18), expected 2 (length-delimited)", wt)
+			}
+			var mm EmbeddedEvent
+			if err = dec.DecodeNested(&mm); err != nil {
+				return fmt.Errorf("unable to decode message value for field 'optional_message' (tag=18): %w", err)
+			}
+			m.OptionalMessage = &mm
+
+		default:
+			if skipped, err := dec.Skip(tag, wt); err != nil {
+				return fmt.Errorf("invalid operation skipping tag %v: %w", tag, err)
+			} else {
+				m.unknownFields = append(m.unknownFields, skipped...)
+			}
+		}
+	}
+	return nil
+}
+
+//------------------------------------------------------------------------------
 // Custom Protobuf size/marshal/unmarshal code for I18NVariable
 
 // Size calculates and returns the size, in bytes, required to hold the contents of m using the Protobuf
